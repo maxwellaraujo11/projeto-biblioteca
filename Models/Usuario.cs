2757065d99acb18d1b1c6103211819585e607d56
maxwellaraujo <maxwellaraujo11@hotmail.com>
2023-04-19 11:13:17 -0300

    criacao da tabela usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Usuario
+    {
+        public int Id {get; set;}
+
+        public string Login {get; set;}
+
+        public string Senha {get; set;}
+    }
+}
